4f31728d788700fefded60836079cea11e8303e0
OmarVerdugo <43006059+OmarVerdugo@users.noreply.github.com>
2019-04-15 19:37:24 -0600

    se creó pagina inicial

---
+++ start.php
@@ -0,0 +1,36 @@
+<?php include('header.php'); ?>
+
+<link rel="stylesheet" type="text/css" href="css/start.css">
+
+	 <section class="startPage">
+		<div class="progreso">   
+		  <ul>
+         	<h3 class="titulo"><li>Bienvenido: [usuario]</li></h3>
+			<h3 class="titulo"><li>Progreso del curso.</li></h3>
+			<div style="float: left; width: 50%;">
+			<li><strong>Capitulo 1 <meter min="0" max="100" low="26" high="75" optimum="100" value="50"></strong></li>
+			<li><strong>Capitulo 2 <meter min="0" max="100" low="26" high="75" optimum="100" value="75"></strong></li>
+			<li><strong>Capitulo 3 <meter min="0" max="100" low="26" high="75" optimum="100" value="75"></strong></li>
+			<li><strong>Capitulo 4 <meter min="0" max="100" low="26" high="75" optimum="100" value="75"></strong></li>
+			</div>
+			<div style="float: right; width: 50%;">
+			<li><strong>Capitulo 5 <meter min="0" max="100" low="26" high="75" optimum="100" value="75"></strong></li>
+			<li><strong>Capitulo 6 <meter min="0" max="100" low="26" high="75" optimum="100" value="25"></strong></li>
+			<li><strong>Capitulo 7 <meter min="0" max="100" low="26" high="75" optimum="100" value="75"></strong></li>
+			<li><strong>Capitulo 8 <meter min="0" max="100" low="26" high="75" optimum="100" value="75"></strong></li>
+			</div>
+
+
+		</ul>
+		</div>
+
+		</section>
+
+	<footer class="footer"><a href="#" id="logout">Cerrar Sesión</a></footer>
+
+		
+
+
+</body>
+
+</html>
