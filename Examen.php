78318a5546b85f685a628f799274d539cd910bbb
OmarVerdugo <43006059+OmarVerdugo@users.noreply.github.com>
2019-04-18 19:46:31 -0600

    interfaces Capitulo, Examen y Ajustes CSS

---
+++ Examen.php
@@ -0,0 +1,17 @@
+	<?php include('template/header.php'); ?>
+	<link rel="stylesheet" type="text/css" href="css/exam.css">
+</head>
+<body>
+	<section class="examen">
+					<form class="panelExamen" action="sesion.php" method="POST">
+					<div class="divDatos">
+						<ul>
+							<li><h3>Capitulo X: </h3></li>
+							<li><h3>Intentos : </h3></li>
+						</ul>
+					</div>
+					<button class="btnFinalizarExamen">Finalizar</button>
+				</form>
+	</section>
+</body>
+</html>
