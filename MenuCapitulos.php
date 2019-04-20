502d4ff296b0a36b5dfdd72ff81b7f8e28acddf9
OmarVerdugo <43006059+OmarVerdugo@users.noreply.github.com>
2019-04-20 16:24:17 -0600

    Se creó la linea de tiempo de los capitulos

---
+++ MenuCapitulos.php
@@ -0,0 +1,60 @@
+	<?php include('template/header.php'); ?>
+	<link rel="stylesheet" type="text/css" href="css/menuChapters.css">
+</head>
+<body>
+	<section class="capitulo">
+		<h3>Capitulos: </h3>
+					
+		<ul class="cbp_tmtimeline">
+    <li>
+        <div class="cbp_tmicon cbp_tmicon-phone"><button class="btnCap">1</button></div>
+        <div class="cbp_tmlabel">
+           
+        </div>
+    </li>
+   <li>
+        <div class="cbp_tmicon cbp_tmicon-phone"><button class="btnCap">2</button></div>
+        <div class="cbp_tmlabel">
+           
+        </div>
+    </li>
+    <li>
+        <div class="cbp_tmicon cbp_tmicon-phone"><button class="btnCap">3</button></div>
+        <div class="cbp_tmlabel">
+           
+        </div>
+    </li>
+    <li>
+        <div class="cbp_tmicon cbp_tmicon-phone"><button class="btnCap">4</button></div>
+        <div class="cbp_tmlabel">
+           
+        </div>
+    </li>
+    <li>
+        <div class="cbp_tmicon cbp_tmicon-phone"><button class="btnCap">5</button></div>
+        <div class="cbp_tmlabel">
+           
+        </div>
+    </li>
+    <li>
+        <div class="cbp_tmicon cbp_tmicon-phone"><button class="btnCap">6</button></div>
+        <div class="cbp_tmlabel">
+           
+        </div>
+    </li>
+    <li>
+        <div class="cbp_tmicon cbp_tmicon-phone"><button class="btnCap">7</button></div>
+        <div class="cbp_tmlabel">
+           
+        </div>
+    </li>
+    <li>
+        <div class="cbp_tmicon cbp_tmicon-phone"><button class="btnCap">8</button></div>
+        
+    </li>
+</ul>
+
+	</section>
+	
+</body>
+</html>
