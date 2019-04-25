b409d4fce4fcc4408e3b49334c861c6a5e8016b2
Ramon Mora <jorainzu@hotmail.com>
2019-04-25 10:25:09 -0600

    Update ModificarAlumno.php
    antes de morir option select query

--- ModificarAlumno.php
+++ ModificarAlumno.php
@@ -9,7 +9,7 @@
 		<section class="studentCont">
 			<div class="studentLabel">
 				<h3 class="encabezado">Modificación de Alumno</h3>
-				<h3>Número de control</h3>
+				<h3>Alumno</h3>
 				<h3>Nombre(s)</h3>	
 				<h3>Apellido paterno</h3>
 				<h3>Fecha de nacimiento</h3>		
@@ -18,9 +18,26 @@
 					<button id="Modificar" class="enviar formBoton" >Registrar</button>
 			</div>
 			<div class="studentInput">
-				<?php #<input type="text" name="" id="_IDControlAlu"> ?>
-				<select class="idAl">
-					<option value=" " selected "     "> </option>
+			
+				<select class="idAl">	
+					<?php 
+					  $stmt = $conn->prepare("SELECT idProfe FROM Profesores WHERE emailPro = :email");
+                      $stmt->bindParam(':email',$_SESSION['user']);
+                      $stmt->execute();
+                      $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                      $profe = $stmt->fetch(); 
+
+                      $stmt = $conn->prepare("SELECT idAlumno, nombreAlu, apePatAlu, apeMatAlu FROM Alumnos WHERE idProfe = :idprofe");
+                      $stmt->bindParam(':idprofe',$profe['idProfe']);
+                      $stmt->execute();
+                      $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                      $nombre = $stmt->fetch();              
+                     
+
+                      echo "<option value=".$nombre['idAlumno']." selected=".$nombre['nombreAlu'].$nombre['apePatAlu']. 
+                      $nombre['apeMatAlu'].">".$nombre['nombreAlu']." ".$nombre['apePatAlu']." " .$nombre['apeMatAlu']. "</option>";         
+				?>
+					<option value=" " selected = ""> </option>
 				</select>
 				<input type="text" name="" id="_nombreAlu">
 				<div class="mismoFlex apeInput">
