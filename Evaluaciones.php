d7f04968241d8dfb8bdc1b3f92c3d44d91e22f58
JMorInzunza <43210606+JMorInzunza@users.noreply.github.com>
2019-04-29 05:57:26 -0600

    interfaz de evaluacion
    se agregaron las paginas de las evaluaciones

--- Evaluaciones.php
+++ Evaluaciones.php
@@ -7,19 +7,28 @@
 	  <ul>
      	
 		<div style="float: left; width: 40%;">
-		<li><strong>Capitulo 1: <input type="text" disabled name="" id="examen1"><br><a href="" id="link">Realizar examen</a></strong></li>
-		<li><strong>Capitulo 2: <input type="text" disabled name="" id="examen1"><br><a href="" id="link">Realizar examen</a></strong></li>
-		<li><strong>Capitulo 3: <input type="text" disabled name="" id="examen1"><br><a href="" id="link">Realizar examen</a></strong></li>
-		<li><strong>Capitulo 4: <input type="text" disabled name="" id="examen1"><br><a href="" id="link">Realizar examen</a></strong></li>
+		<li><strong>Capitulo 1: <input type="text" disabled name="" id="examen1"><br>
+			<a href="http://localhost/ProyectoIS2/examenCap1.php#" id="link">Realizar examen</a></strong></li>
+		<li><strong>Capitulo 2: <input type="text" disabled name="" id="examen1"><br>
+			<a href="http://localhost/ProyectoIS2/examenCap2.php#" id="link">Realizar examen</a></strong></li>
+		<li><strong>Capitulo 3: <input type="text" disabled name="" id="examen1"><br>
+			<a href="http://localhost/ProyectoIS2/examenCap3.php#" id="link">Realizar examen</a></strong></li>
+		<li><strong>Capitulo 4: <input type="text" disabled name="" id="examen1"><br>
+			<a href="http://localhost/ProyectoIS2/examenCap4.php#" id="link">Realizar examen</a></strong></li>
 		</div>
 		<div class="mitad" style="float: left; width:30%;">
-		<li><strong>Capitulo 5: <input type="text" disabled name="" id="examen1"><br><a href="" id="link">Realizar examen</a></strong></li>
-		<li><strong>Capitulo 6: <input type="text" disabled name="" id="examen1"><br><a href="" id="link">Realizar examen</a></strong></li>
-		<li><strong>Capitulo 7: <input type="text" disabled name="" id="examen1"><br><a href="" id="link">Realizar examen</a></strong></li>
-		<li><strong>Capitulo 8: <input type="text" disabled name="" id="examen1"><br><a href="" id="link">Realizar examen</a></strong></li>
+		<li><strong>Capitulo 5: <input type="text" disabled name="" id="examen1"><br>
+			<a href="http://localhost/ProyectoIS2/examenCap5.php#" id="link">Realizar examen</a></strong></li>
+		<li><strong>Capitulo 6: <input type="text" disabled name="" id="examen1"><br>
+			<a href="http://localhost/ProyectoIS2/examenCap6.php#" id="link">Realizar examen</a></strong></li>
+		<li><strong>Capitulo 7: <input type="text" disabled name="" id="examen1"><br>
+			<a href="http://localhost/ProyectoIS2/examenCap7.php#" id="link">Realizar examen</a></strong></li>
+		<li><strong>Capitulo 8: <input type="text" disabled name="" id="examen1"><br>
+			<a href="http://localhost/ProyectoIS2/examenCap8.php#" id="link">Realizar examen</a></strong></li>
 		</div>
 		<div class="mitad" style="float: right; width: 30%;">
-		<li><strong>Examen Final:<input type="text" disabled name="" id="examen1"><br><a href="" id="link">Realizar examen</a></strong></li>
+		<li><strong>Examen Final:<input type="text" disabled name="" id="examen1"><br>
+			<a href="http://localhost/ProyectoIS2/examenCapF.php#" id="link">Realizar examen</a></strong></li>
 		</div>
 
 
---
+++ examenCapF.php
@@ -0,0 +1,22 @@
+<?php include('template/header.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Evaluacion Unidad 1</title>
+	<link rel="stylesheet" type="text/css" href="css/eval.css">
+</head>
+<body>
+	<div class="desc">
+		<h3>Examen final</h3>
+		<h3>Intentos: </h3>
+		<?php # numero de intentos pendiente ?>
+	</div>
+
+	<div class="exam">
+		
+
+	</div>
+	<div class="butDis"><button class="final">Finalizar</button></div>
+
+</body>
+</html>
