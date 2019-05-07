da400833a7f827b1d570ae0720412527f90d068c
JMorInzunza <43210606+JMorInzunza@users.noreply.github.com>
2019-05-07 04:37:07 -0600

    Se agregan sopas de letras
    se agregan las sopas de letras

---
+++ CrucigramaCinco.php
@@ -0,0 +1,29 @@
+<?php include("template/header.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Sopa de letras</title>
+  
+   
+    <link rel="stylesheet" type="text/css" href="css/crus.css">
+    <script language="javascript" type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script> 
+    <script type="text/javascript" src="wordfind.js"></script> 
+    <script type="text/javascript" src="wordfindgame.js"></script> 
+</head>
+<body>
+
+    <div id='juego'></div>
+    <div id='Palabras'></div>
+    
+    <script>
+    var words = ['Japon','Fibra','Optica','Geneticos','Robotica','Algoritmos'];
+    var gamePuzzle = wordfindgame.create(words, '#juego', '#Palabras'); 
+        
+    var puzzle = wordfind.newPuzzle(words,{height: 18, width:18, fillBlanks: false});
+    wordfind.print(puzzle);   
+        
+    </script>
+    <button>Finalizar</button>
+    
+</body>
+</html>
--- Juegos.php
+++ Juegos.php
@@ -7,16 +7,16 @@
 		  <ul>
          	
 			<div style="float: left; width: 50%;">
-			<li><strong>Capitulo 1: <br>Crucigrama</strong></li>
-			<li><strong>Capitulo 2: <br>Crucigrama</strong></li>
-			<li><strong>Capitulo 3: <br>Crucigrama</strong></li>
-			<li><strong>Capitulo 4: <br>Crucigrama</strong></li>
+			<li><strong>Capitulo 1: <br><a href="CrucigramaUno.php">Jugar</a></strong></li>
+			<li><strong>Capitulo 2: <br><a href="CrucigramaDos.php">Jugar</a></strong></li>
+			<li><strong>Capitulo 3: <br><a href="CrucigramaTres.php">Jugar</a></strong></li>
+			<li><strong>Capitulo 4: <br><a href="CrucigramaCuatro.php">Jugar</a></strong></li>
 			</div>
 			<div class="mitad" style="float: right; width: 50%;">
-			<li><strong>Capitulo 5: <br>Crucigrama</strong></li>
-			<li><strong>Capitulo 6: <br>Crucigrama</li>
-			<li><strong>Capitulo 7: <br>Crucigrama</li>
-			<li><strong>Capitulo 8: <br>Crucigrama</strong></li>
+			<li><strong>Capitulo 5: <br><a href="CrucigramaCinco.php">Jugar</a></strong></li>
+			<li><strong>Capitulo 6: <br><a href="CrucigramaSeis.php">Jugar</a></li>
+			<li><strong>Capitulo 7: <br><a href="CrucigramaSiete.php">Jugar</a></li>
+			<li><strong>Capitulo 8: <br><a href="CrucigramaOcho.php">Jugar</a></strong></li>
 			</div>
 
 
